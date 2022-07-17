eecea9237fec00ec2d3310499df54b85c51a5dbe
MichalSasula <michalsasula@gmail.com>
2022-07-17 20:07:23 +0200

    widget

--- projektPython/drawChart.py
+++ projektPython/drawChart.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+__education2description = {1: 'podstawówka', 2: 'level2', 3: 'level3', 4: 'level4', 5: 'level5'}
 
 def drawGroupByAndCount(df: pd.DataFrame, column_names, column_values = None) -> pd.DataFrame:
 
@@ -24,6 +25,31 @@ def drawFeatureOverTime(df: pd.DataFrame, x: str, y: str):
     ax.set_xlabel(x)
     ax.set_ylim(ymin=0)
     leg = ax.legend()
+    
+def drawPyplot(filteredData):
+    fig, ax = plt.subplots(figsize=(24,12))
+    size = 0.3
+    
+    innerUnique = filteredData['Education'].unique()
+    innerCounts = filteredData['Education'].value_counts()
+    texts_inner, autotexts_inner = ax.pie(innerCounts, radius=size, 
+       labels = innerUnique,
+       wedgeprops=dict(width=size, edgecolor='w'))
+    
+    outerUnique = filteredData.groupby(['EducationField', 'Education']).size().index
+    outerCounts = filteredData.groupby(['EducationField', 'Education']).size()
+    ax.pie(outerCounts, radius=1-size,
+       labels=[index[0] for index in outerUnique],
+       autopct='%1.1f%%',
+       wedgeprops=dict(width=size, edgecolor='w'))
+    
+    ax.legend([__education2description[level] for level in innerUnique],
+          title="Eductaion level",
+          loc="center left",
+          bbox_to_anchor=(1, 0, 2, 1))
+
+    ax.set(aspect="equal", title='Pie plot with `ax.pie`')
+    plt.show()
 
 def drawHistogramWithYourPosition(filtered_data: pd.DataFrame, column_name: str, your_salary= None, number_of_bins=10):
     plt.hist(filtered_data[column_name], bins = number_of_bins, facecolor='blue', alpha=0.5, ec='black')
@@ -33,4 +59,6 @@ def drawHistogramWithYourPosition(filtered_data: pd.DataFrame, column_name: str,
     else:
         plt.legend(["foo"])
     plt.show()
+    
+
 
--- projektPython/reportTools.py
+++ projektPython/reportTools.py
@@ -1,5 +1,14 @@
 # Contains help-functions for report preparation
 import pandas as pd
+import drawChart as dc
 
 def getByDepartmentJobRoleLevelAndMonthlyIncome(data: pd.DataFrame, department, job_role, job_level, monthly_income):
     return data.loc[(data['Department'] == department) & (data['JobRole'] == job_role) & (data['JobLevel'] == job_level) & (data['MonthlyIncome'] > monthly_income)]
+
+def prepareReport(data: pd.DataFrame, user_department, user_job_role, user_job_level, user_monthly_income):
+    filteredData = getByDepartmentJobRoleLevelAndMonthlyIncome(data, user_department, user_job_role, user_job_level, user_monthly_income)
+    dc.drawHistogramWithYourPosition(data, "MonthlyIncome", user_monthly_income)
+    dc.drawPyplot(filteredData)
+    
+
+    
